60a972bf95aee8404576211e0db3f0bfc2e3e22a
Luan Tran <ducluanxutrieu@gmail.com>
2023-02-11 16:08:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello DevOps class. I'm Luan. Whenever you saw this message, it mean that you install this application sucessfully
         </p>
         <a
           className="App-link"
