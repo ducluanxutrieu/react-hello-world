2ff41477edebd3412aa142355d5b1f2efcd36614
Duc Luan Tran <ducluanxutrieu@gmail.com>
2023-04-24 08:01:21 +0000

    merge file

--- create_k8s.sh
+++ create_k8s.sh
@@ -3,15 +3,15 @@
 echo "Create k8s"
 
 # Create EKS Cluster
-eksctl create cluster -f kubernetes/eks-config.yml
+eksctl create cluster -f kubernetes/eks-config.yml || true
 
 aws eks update-kubeconfig --name helloworld --region us-east-1
-sed -i -e 's/v1alpha1/v1beta1/' ~/.kube/config
 
 # Run kubernetes deployment
-kubectl create -f kubernetes/helloworld-deploy.yaml
+kubectl create -f kubernetes/helloworld-deploy.yaml || kubectl apply -f kubernetes/helloworld-deploy.yaml
 # Run kubernetes service
-kubectl create -f kubernetes/helloworld-service.yaml
+kubectl create -f kubernetes/helloworld-service.yaml || kubectl apply -f kubernetes/helloworld-service.yaml
 
 kubectl get all
 kubectl rollout status deployment/helloworld-deploy
+kubectl rollout history deployment/helloworld-deploy
